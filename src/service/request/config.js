bb761c8edd069dd38265a2f210123f3ff5673c88
Lipigs <93815919+Lipigs@users.noreply.github.com>
2024-03-15 21:17:47 +0800

    Add files via upload

---
+++ src/service/request/config.js
@@ -0,0 +1,8 @@
+// 服务器
+// export const BASE_URL = "http://124.71.180.8:19001/";
+// 微信云托管
+
+export const BASE_URL ="https://facility-89788-7-1307807477.sh.run.tcloudbase.com/";
+//  本地
+// export const BASE_URL = "http://localhost:19001/";
+export const OUTTIME = 5000;
